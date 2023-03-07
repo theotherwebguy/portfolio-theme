f1cc60eb9cc6268ad538121c46180b9148c65da7
Msizi Ndlela <msizi.ndlela5@gmail.com>
2023-03-07 21:17:18 +0200

    adding theme screenshot

--- functions.php
+++ functions.php
@@ -5,3 +5,12 @@
  * @package portfolio
  * 
  */
+
+//  adding theme styleshetts
+function portfolio_enqueue_scripts(){
+    wp_enqueue_style( 'portfolio_style', get_template_directory_uri() . 'style.css',[], filemtime(get_template_directory_uri(  ) . 'style.css'), 'all' );
+
+    wp_enqueue_script( 'portfolio_js', 
+    get_template_directory_uri( ) . '/assets/js/main.js',[], filemtime(), true );
+}
+add_action( 'wp_enqueue_scripts','portfolio_enqueue_scripts' );
